784a7881da4bf4e2a898010fd2818ded00e33e54
Hagen1999 <schmilgeit@virtualform.de>
2022-05-16 12:09:10 +0000

    Test Simple Statisics Install

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
+<script>
+
+import {min} from 'simple-statistics'
+
+let abc =  [1, 5, -10, 100, 2];
+const xyz = min(abc); // => -10
+
+</script>
+
 <h1>Welcome to SvelteKit by svs</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div> Das Array besteht aus: {abc} </div>
+<div> Das Ergebnis "Min" lautet: {xyz} </div>
+
