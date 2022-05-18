51383596be5370a83b2d559029b8155c254d87e8
Hagen1999 <schmilgeit@virtualform.de>
2022-05-18 13:34:01 +0000

    Lesbarkeit und Regression

--- src/speicher/store.js
+++ src/speicher/store.js
@@ -1,5 +1,5 @@
 import { writable } from "svelte/store";
 
-export const datenreihe1 = writable([50,25,10,5,5,5]);// Beispieldaten
+export const datenreihe1 = writable([50,25,10,5,5,5]);// Beispieldaten 1
 export const datenreihe2 = writable([55,20,14,4,6,6]);// Beispieldaten 2
-export const tortenfarben1 = writable(["Rot", "Grün", "Gelb", "Grau", "Dunkelgrau"]);// Beispieldaten
+export const tortenfarben1 = writable(["Rot", "Grün", "Gelb", "Grau", "Dunkelgrau"]); // Beispieldaten Legende
