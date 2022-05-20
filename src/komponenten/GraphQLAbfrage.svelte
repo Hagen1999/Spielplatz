849a1931f405cc4a7e02ed3063fa122ecf331644
Hagen1999 <schmilgeit@virtualform.de>
2022-05-20 15:55:56 +0000

    graphql code verändert

--- src/komponenten/GraphQLAbfrage.svelte
+++ src/komponenten/GraphQLAbfrage.svelte
@@ -2,12 +2,12 @@
 import { GraphQLClient, gql } from 'graphql-request'
 
 async function main() {
-  const endpoint = 'https://eu-central-1.aws.realm.mongodb.com/api/client/v2.0/app/vftestapp0-mhegz/graphql'
+  const endpoint = '...'
 
   const graphQLClient = new GraphQLClient(endpoint, {
     headers: {
-      authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiYWFzX2RldmljZV9pZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsImJhYXNfZG9tYWluX2lkIjoiNjI0MWM0N2NmMmFlYTg2YmFkOTYwYTA3IiwiZXhwIjoxNjUzMDYxMTY0LCJpYXQiOjE2NTMwNTkzNjQsImlzcyI6IjYyODdhZjI0Y2Q4MGI1NmRhNmI1NjMzNiIsInN0aXRjaF9kZXZJZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsInN0aXRjaF9kb21haW5JZCI6IjYyNDFjNDdjZjJhZWE4NmJhZDk2MGEwNyIsInN1YiI6IjYyODdhZjI0Y2Q4MGI1NmRhNmI1NjMzNCIsInR5cCI6ImFjY2VzcyJ9.HDRZzUobsCeTc-oXNHbaLbORdWZ3YjVmulXfg01Ylqo',
-        //apiKey: 'tSZzomqB98AePyPGALsLzahodck5BpNgGZYbszR1mU3TQqQwiWJmPQKcyRXAkTMV',
+      //authorization: 'Bearer ...',
+        //apiKey: '...',
     },
   })
 
