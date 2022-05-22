7a5195552c7df440b904f3548b8a47fb02359904
Hagen1999 <schmilgeit@virtualform.de>
2022-05-22 09:52:27 +0000

    tests von Variablen

--- src/komponenten/GraphQLAbfrage.svelte
+++ src/komponenten/GraphQLAbfrage.svelte
@@ -1,12 +1,12 @@
 <script>
 import { GraphQLClient, gql } from 'graphql-request'
 
-async function main() {
-  const endpoint = '...'
+export async function main() {
+  const endpoint = ''
 
   const graphQLClient = new GraphQLClient(endpoint, {
     headers: {
-      //authorization: 'Bearer ...',
+      authorization: '',
         //apiKey: '...',
     },
   })
@@ -30,12 +30,33 @@ async function main() {
 
   const data = await graphQLClient.request(query);
   console.log(JSON.stringify(data, undefined, 2));
-  
+  //console.log("toll2");
+  //console.log(JSON.stringify(data));
 
-  const final = JSON.stringify(data);
+  const final = JSON.stringify(data, undefined, 2);
+
+  const jahr0 = JSON.stringify(data.abfrageJahr[0].Jahr);
+  const jahr1 = JSON.stringify(data.abfrageJahr[1].Jahr);
+  const jahr2 = JSON.stringify(data.abfrageJahr[2].Jahr);
+  const jahre = "Jahre: " + jahr0 + jahr1 +  jahr2;
+
+  console.log(JSON.stringify(data.abfrageJahr[1].Jahr));
+  console.log("Jahre: " + jahr0, jahr1, jahr2);
+  console.log("Jahre als String: " + jahre);
+
   return final;
-  //return data;
 }
+
+/*
+main().then((final2) => {
+  //const title = document.querySelector('h1');
+  //title.textContent = final2;
+  //const resultatAbfrage = final;
+  const final3 = JSON.stringify(final2);
+  //console.log("die Antwort: " + final3 + " war das!");
+  return final3;
+});
+*/
 
 main().catch((error) => console.error(error));
 //const resultatAbfrage = main().catch((error) => console.error(error));
@@ -49,5 +70,5 @@ main().catch((error) => console.error(error));
 <slot></slot>
 
 <div>
-    Das JSON-Objekt die Antwort lautet: {main().catch()}.
+    Das JSON-Objekt die Antwort lautet: .
 </div>
