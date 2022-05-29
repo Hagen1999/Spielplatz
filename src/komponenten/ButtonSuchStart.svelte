b1c6405017168d85348ccdb544b35908f03392db
Hagen1999 <schmilgeit@virtualform.de>
2022-05-29 19:20:31 +0000

    Komponenten übergreifende Befehle

--- src/komponenten/ButtonSuchStart.svelte
+++ src/komponenten/ButtonSuchStart.svelte
@@ -1,12 +1,15 @@
 <script>
+	import {createEventDispatcher} from "svelte";
+
+	const dispatch = createEventDispatcher();
 
 	const handleClick = () => {
-		console.log('Button wurde geklickt.')
+		console.log('Button wurde in der Button-Komponente geklickt.')
+		dispatch("Signal");
 	}
 
 </script>
 
 <button 
-	on:click= {handleClick}>
-	Testbuton - Klicktest
+	on:click= {handleClick}>Testbuton - Klicktest
 </button>
