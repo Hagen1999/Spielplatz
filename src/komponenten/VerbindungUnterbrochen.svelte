193a102a321ee2e8185bfd33745e01bd842d259d
Hagen1999 <schmilgeit@virtualform.de>
2022-06-09 17:32:05 +0000

    Login Komponenten geändert

--- src/komponenten/VerbindungUnterbrochen.svelte
+++ src/komponenten/VerbindungUnterbrochen.svelte
@@ -2,5 +2,5 @@
     import Login from "./Login.svelte";
 </script>
 
-<p>Die Verbindung wurde unterbrochen. Bitte melden Sie sich neu an.</p>
+<p>User ist nicht eingeloggt. Die Verbindung wurde unterbrochen. Bitte melden Sie sich neu an.</p>
 <Login></Login>
