f8c4cae7dddc3fc99c117d680d0df29f2de2908e
Hagen1999 <schmilgeit@virtualform.de>
2022-06-09 17:08:29 +0000

    Login Komponenten geordnet

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,34 +2,37 @@
     /*
     import Kuchendiagramm from '../komponenten/Kuchendiagramm.svelte';
     import Eingabeformular from '../komponenten/Eingabeformular.svelte';
-
     import JahreszahlAuswahl from '../komponenten/JahreszahlAuswahl.svelte';
     import Statistik from '../komponenten/Statistik.svelte';
     import Login from '..//komponenten/Login.svelte';
-*/
-    import JahreszahlAuswahl from '../komponenten/JahreszahlAuswahl.svelte';
-    import GraphQlAbfrage from '../komponenten/GraphQLAbfrage.svelte';
-    import LoginComponent from '../komponenten/LoginComponent.svelte';
-    import Login from '..//komponenten/Login.svelte';
-    
+    */
+
+    //import JahreszahlAuswahl from '../komponenten/JahreszahlAuswahl.svelte';
+    //import GraphQlAbfrage from '../komponenten/GraphQLAbfrage.svelte';
+    import {benutzerID} from '../speicher/store'
+    import {benutzerEingeloggt} from '../speicher/store'
+    //import LoginComponent from '../komponenten/LoginComponent.svelte';
+    //import AnmeldeStatus from '../komponenten/AnmeldeStatus.svelte';
+    import Login from '../komponenten/Login.svelte';
 
 </script>
 
 <svelte:head>
 </svelte:head>
 
-<main>
+
 <h1>Index-Seite</h1>
-
-<a href="/filter">Link Filterseite</a>
-
+<p>Eingeloggt? {$benutzerEingeloggt}</p>
+<p>Benutzer-ID: {$benutzerID}</p>
+<hr>
 <Login/>
+<!-- HTML 
 <br/>
 <JahreszahlAuswahl></JahreszahlAuswahl>
 <br/>
 <GraphQlAbfrage></GraphQlAbfrage>
 
-<!-- HTML 
+
 <a href = "/login"> Logintestseite</a>
 <br/>
 <Login></Login>
@@ -44,4 +47,3 @@
 <br/>
 <Kuchendiagramm></Kuchendiagramm>
 -->
-</main>
